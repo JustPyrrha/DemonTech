1274be8413d4e69edfdcbe521932cbfc8bd3216c
Pyrrha <pyrrha.social@gmail.com>
2024-01-07 15:44:08 +1300

    feat: 2024 license header update

--- libraries/sync/src/main/kotlin/gay/pyrrha/sync/converter/LongNbtConverter.kt
+++ libraries/sync/src/main/kotlin/gay/pyrrha/sync/converter/LongNbtConverter.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Pyrrha Wills
+ * Copyright 2023-2024 Pyrrha Wills
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
